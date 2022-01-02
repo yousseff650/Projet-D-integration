0e1febae960ec603abbe9a22de0708a310b62077
ahmed stanbouli <ahmed.stambouli98@gmail.com>
2022-01-02 19:59:15 +0100

    modif les nav bar

--- app/src/main/java/com/example/projetdintergration/RegisterActivity.java
+++ app/src/main/java/com/example/projetdintergration/RegisterActivity.java
@@ -2,10 +2,12 @@ package com.example.projetdintergration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,6 +17,8 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button btnRegister;
+        btnRegister=findViewById(R.id.register);
         getSupportActionBar().hide();
         // spinner departement
         Spinner spinner = findViewById(R.id.departement);
@@ -29,6 +33,13 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
         spinner1.setOnItemSelectedListener(this);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),login.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
